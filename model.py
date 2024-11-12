64c64c126b2572f8dec308128eeab3874130cb4f
WangEntang <maxewang10@gmail.com>
2024-11-12 10:30:41 +0100

    24.11.12 add base transformer block

---
+++ model.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+#define transformer block
+class TransformerBlock(nn.Module):
+    def __init__(self, embedding_size, num_heads, forward_expansion, dropout):
+        super(TransformerBlock, self).__init__()
+
+        # multi-head attention
+        self.attention = nn.MultiheadAttention(embedding_size, num_heads, dropout=dropout)
+
+        # add & norm
+        self.norm1 = nn.LayerNorm(embedding_size)
+        self.norm2 = nn.LayerNorm(embedding_size)
+
+        # feed forward
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embedding_size, forward_expansion * embedding_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embedding_size, embedding_size)
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        # shape of x: (sequence_length, batch_size, embedding_size)
+        # adjust the shape
+        x = x.transpose(0, 1)
+
+        # input x to multi-head attention
+        attention_output, _ = self.attention(x, x, x, attn_mask=mask)
+
+        # residual connection and dropout
+        x = x + self.dropout(attention_output)
+
+        #normalization
+        x = self.norm1(x)
+
+        # feed forward
+        forward_output = self.feed_forward(x)
+
+        # residual connection and normalization
+        out = self.norm2(x + self.dropout(forward_output))
+
+        return out.transpose(0, 1)

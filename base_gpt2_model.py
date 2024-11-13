a458e9a44da286521cdb25785b94dac99166ebf7
WangEntang <maxewang10@gmail.com>
2024-11-13 23:46:18 +0100

    24.11.13 adjust some parameters of the baseline GPT2 model

--- base_gpt2_model.py
+++ base_gpt2_model.py
@@ -5,11 +5,19 @@ model = GPT2LMHeadModel.from_pretrained(MODEL_NAME)
 tokenizer = GPT2Tokenizer.from_pretrained(MODEL_NAME)
 
 # input text
-input_text = "Once upon a time"
-input_ids = tokenizer.encode(input_text, return_tensors='pt')
+input_text = "A spectre is haunting Europe — the spectre of"
+
+# encode text
+inputs = tokenizer(input_text, return_tensors='pt')
+input_ids = inputs["input_ids"]
+attention_mask = inputs["attention_mask"]
 
 # generate text
-output = model.generate(input_ids, max_length=100, num_return_sequences=1)
+output = model.generate(input_ids, attention_mask= attention_mask, max_length=50,
+                        num_return_sequences=1,
+                        pad_token_id=tokenizer.eos_token_id,
+                        eos_token_id=tokenizer.eos_token_id,
+                        repetition_penalty=1.2)
 
 # decode text
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
